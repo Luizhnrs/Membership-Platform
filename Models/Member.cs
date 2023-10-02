7dafb5ecdf9f46afb0f4d625b086ebeb98221c38
Luizhnrs <Luizhnrsg@gmail.com>
2023-10-02 19:47:10 -0300

    MembersInput

---
+++ Models/Member.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace PlataformaDeMembros.Models;
+
+public class Member
+{
+     
+    [DisplayName("Id")] 
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Informe o Nome")]
+    [StringLength(80, ErrorMessage = "O Nome Deve Conter ate 80 Caracteres")]
+    [MinLength(5, ErrorMessage = "O Nome Deve Conter no Minimo 5 Caracteres")]
+    [DisplayName("Nome Completo")]
+    public string Name { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Informe o Email")]
+    [EmailAddress(ErrorMessage = "Email Invalido")]
+    [DisplayName("E-Mail")]
+    public string Email { get; set; } = string.Empty;
+    
+
+    public List<PremiumMembers> Premium { get; set; } = new();
+}
---
+++ Models/PremiumMembers.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace PlataformaDeMembros.Models;
+
+public class PremiumMembers
+{
+    [Key] [DisplayName("id")] public int id { get; set; }
+
+    [Required(ErrorMessage = "Informe o titulo do Premium")]
+    [StringLength(80, ErrorMessage = "O titulo deve conter ate 80 caracteres")]
+    [MinLength(5, ErrorMessage = "O titulo deve conter no minimo 5 caracteres")]
+
+    [DisplayName("Titulo")]
+    public string Title { get; set; } = string.Empty;
+    
+    [DataType(DataType.DateTime)]
+    [DisplayName("Inicio")]
+    public DateTime StartDate { get; set; }
+    
+    [DataType(DataType.DateTime)]
+    [DisplayName("Termino")]
+    public DateTime EndDate { get; set; }
+    
+    [DisplayName("Membro")]
+    [Required(ErrorMessage = "Membro Invalido")]
+    public int MemberId { get; set; }
+    
+    public Member? Member { get; set; }
+}
